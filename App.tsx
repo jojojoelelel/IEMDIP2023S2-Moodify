2007db8b572515bae554897a59f1508cc019b8d8
sneham88 <156883165+sneham88@users.noreply.github.com>
2024-02-14 14:37:34 +0800

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import MainContainer from './navigation/MainContainer';
+import 'react-native-gesture-handler';
+
+
+function App(){
+    return(
+        <MainContainer/>
+    );
+}
+
+export default App;
+
+
+
+
