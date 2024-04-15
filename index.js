4dda5fe475f96a5c3c55d0d803b441c25b2e9864
jojojoelelel <JoelHFJ7643@gmail.com>
2024-04-15 20:48:44 +0800

    firebase to use latest versions

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import firebase from '@react-native-firebase/app';
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_I,
+  projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   appId: process.env.FIREBASE_APP_ID,
 };
